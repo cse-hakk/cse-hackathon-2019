3d77f48209b8ef62d3aa8af5613ea2c087059f21
Kevin Lin <z5162055@student.unsw.edu.au>
2019-07-27 17:07:54 +1000

    stuff

---
+++ src/Attraction.py
@@ -0,0 +1,9 @@
+
+class Attraction:
+    def __init__(self):
+        self._title = title
+        self._country = country
+        self._city = city
+        self._description = description
+        self._consumer_rating = consumer_rating
+        self._sustain_rating = sustain_rating
