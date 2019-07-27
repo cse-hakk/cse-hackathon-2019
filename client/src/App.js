3f6921b48d61de7ccd1210c0d80125157438f46e
Thomas Lo <tlo3068@gmail.com>
2019-07-27 20:44:45 +1000

    refresh

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,4 @@
 import React from "react";
-import {
-  Layout,
-  Menu,
-  Breadcrumb,
-  Icon,
-  Avatar,
-  Typography,
-  Divider
-} from "antd";
-
-import SummaryCard from "./components/SummaryCard";
-import "./App.css";
-
-const { Header, Content, Footer } = Layout;
-const { Title, Paragraph, Text } = Typography;
 
 function App() {
   return <div className="App" />;
