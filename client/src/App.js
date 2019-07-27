753042b1786d9cbe07104edc677dd5635870554e
Miran Nakamura <miran@Mirans-MacBook-Pro.local>
2019-07-27 22:59:50 +1000

    header? does it work?

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
+import { Divider, Dropdown, Image, Button, Segment } from 'semantic-ui-react'
 import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
 import Search from "./components/Search/Search";
@@ -20,7 +21,22 @@ const DetailsPage = () => {
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Segment>
+        <Header as='h1' floated='left'>
+          <Image size='small' src='https://cdn3.iconfinder.com/data/icons/global-earth-navigation-location-1/100/51-512.png' avatar />
+          <span>EcoTrip</span>
+          <Dropdown
+          button
+          className='icon'
+          floating
+          labeled
+          icon='world'
+          search
+          text='View your itinerary'
+          />
+          <Button content="View your itinerary" icon={{ color: 'white', name: 'view' }} />
+        </Header>
+      </Segment>
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/search" component={SearchPage} />
