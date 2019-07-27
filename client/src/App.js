4db005f0fb120e8f491aba6b961b5072b7e77509
Thomas Lo <tlo3068@gmail.com>
2019-07-27 20:44:02 +1000

    removed everything

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,22 @@
-import React from 'react';
-import { Layout, Menu, Breadcrumb, Icon, Avatar, Typography, Divider } from 'antd';
+import React from "react";
+import {
+  Layout,
+  Menu,
+  Breadcrumb,
+  Icon,
+  Avatar,
+  Typography,
+  Divider
+} from "antd";
 
-import SummaryCard from './components/SummaryCard';
-import './App.css';
+import SummaryCard from "./components/SummaryCard";
+import "./App.css";
 
 const { Header, Content, Footer } = Layout;
 const { Title, Paragraph, Text } = Typography;
 
-
 function App() {
-  return (
-    <div className="App">
-      <Layout>
-      <Header className="App-header">
-      <Menu
-      backgroundColor="747474"
-      mode="horizontal"
-      defaultSelectedKeys={['2']}
-      style={{ lineHeight: '64px' }}>
-        <Avatar size={60} style={{ backgroundColor: '#90EE90' }} src="https://cdn3.iconfinder.com/data/icons/global-earth-navigation-location-1/100/51-512.png" />
-        <Text strong>EcoTrip</Text>
-        <Menu.Item key="1">View Itinerary</Menu.Item>
-
-      </Menu>
-    </Header>
-        <Content>
-          <SummaryCard/>
-        </Content>
-      </Layout>
-      <Footer/>
-    </div>
-  );
+  return <div className="App" />;
 }
 
 export default App;
