0bd0251786fd44f2b54e3b46e40735e40b243c4d
Thomas Lo <tlo3068@gmail.com>
2019-07-27 21:05:00 +1000

    refactor

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,34 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
+import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+import Search from "./components/Search/Search";
+import Details from "./components/Details/Details";
+
+const HomePage = () => {
+  return <Home />;
+};
+
+const SearchPage = () => {
+  return <Search />;
+};
+
+const DetailsPage = () => {
+  return <Details />;
+};
 
 function App() {
-  return <div className="App" />;
+  return (
+    <div className="App">
+      {/* <Header /> */}
+      {/* <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/search" component={SearchPage} />
+        <Route path="/details" component={DetailsPage} />
+      </Switch> */}
+      <div>Hello world</div>
+    </div>
+  );
 }
 
 export default App;
---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import { Container } from "semantic-ui-css";
+
+class Header extends Component {
+  state = {};
+  render() {
+    return <Container>header</Container>;
+  }
+}
+
+export default Header;
