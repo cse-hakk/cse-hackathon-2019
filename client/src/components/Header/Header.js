919d433b11394e262eaa4a8301dd060cf29ea652
N00bcode <z5163243@unsw.edu.au>
2019-07-28 03:47:14 +1000

    Itinerary

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,29 +1,31 @@
 import React from "react";
-import { Segment, Image, Header, Dropdown, Button } from "semantic-ui-react";
+import { Link } from "react-router-dom";
+import { Segment, Image, Header, Dropdown, Button, GridRow, GridColumn } from "semantic-ui-react";
 
 export const MyHeader = props => {
   return (
     <Segment>
-      <Header as="h1" floated="left" />
-      <Image
+      {/* <Grid>
+        <Grid.Row>
+          <Grid.Column>
+          <Header as="h1" floated="left" />
+          </Grid.Column>
+          <Grid.Column></Grid.Column>
+          <Grid.Column></Grid.Column>
+        </Grid.Row>
+      </Grid> */}
+      <Link to='/'> <Image
         size="tiny"
         src="https://cdn3.iconfinder.com/data/icons/global-earth-navigation-location-1/100/51-512.png"
         avatar
+      /></Link>
+      <span style={{marginLeft:'30px', fontSize:'45px', verticalAlign:'middle'}}>EcoTrip</span>
+      <Link to="/itinerary"> 
+      <Button size='big' style={{float:"right", marginTop:'20px'}}
+        icon={{ color: "grey  ", name: "calendar"}}
       />
-      <span>EcoTrip</span>
-      <Dropdown
-        button
-        className="icon"
-        floating
-        labeled
-        icon="world"
-        search
-        text="View your itinerary"
-      />
-      <Button
-        content="View your itinerary"
-        icon={{ color: "white", name: "view" }}
-      />
+      </Link>
+      
     </Segment>
   );
 };
