39be50f71c5ff9de4a2611baddc11135c2cb4b70
Thomas Lo <tlo3068@gmail.com>
2019-07-28 00:50:52 +1000

    header revert

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,23 +1,29 @@
 import React from "react";
-import { Header, Menu, Avatar, Text } from "semantic-ui-react";
+import { Segment, Image, Header, Dropdown, Button } from "semantic-ui-react";
 
 export const MyHeader = props => {
   return (
-    <Header className="App-header">
-      <Menu
-        backgroundColor="747474"
-        mode="horizontal"
-        defaultSelectedKeys={["2"]}
-        style={{ lineHeight: "64px" }}
-      >
-        <Avatar
-          size={60}
-          style={{ backgroundColor: "#90EE90" }}
-          src="https://cdn3.iconfinder.com/data/icons/global-earth-navigation-location-1/100/51-512.png"
-        />
-        <Text strong>EcoTrip</Text>
-        <Menu.Item key="1">View Itinerary</Menu.Item>
-      </Menu>
-    </Header>
+    <Segment>
+      <Header as="h1" floated="left" />
+      <Image
+        size="tiny"
+        src="https://cdn3.iconfinder.com/data/icons/global-earth-navigation-location-1/100/51-512.png"
+        avatar
+      />
+      <span>EcoTrip</span>
+      <Dropdown
+        button
+        className="icon"
+        floating
+        labeled
+        icon="world"
+        search
+        text="View your itinerary"
+      />
+      <Button
+        content="View your itinerary"
+        icon={{ color: "white", name: "view" }}
+      />
+    </Segment>
   );
 };
